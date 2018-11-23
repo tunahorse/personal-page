f35d7734f7f6129de8739da1de1c1b095c7525d2
Fabian Anguiano <fabiananguiano@gmail.com>
2018-11-22 22:45:32 -0600

    new year new port

---
+++ src/data.js
@@ -0,0 +1,109 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "https://github.com/thetrebelcc"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": ""
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/feed/"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "f665eb6a-b796-423a-b566-851d8b227b36",
+            "title": "Contact Center Representative at Appfolio",
+            "timeline": "Dec 2017 - Present",
+            "description": "",
+            "responsibilities": [
+                "Communicate with customers over the phone while creating reports utilizing salesforce internal software.\n\n",
+                "Monitor, evaluate, respond and follow-up to customer inquiries and requests\n\n",
+                "Resolve general inquiries and complex issues both autonomously and in a team environment.",
+                "\nProactively evaluate and report any potential issues with service."
+            ]
+        },
+        {
+            "__key": "d626c384-ca98-457a-bd08-bd1ed8eb820f",
+            "title": " Freelance Web Developer",
+            "timeline": " Dec 2017-Present",
+            "responsibilities": [
+                "Consulted with client to outline website goals, illustrate wire frame, and site functionality\n",
+                "Developed a user-friendly architecture by programming site navigation, social media links/widgets, and organizing site content into a logical catalog\n",
+                "Validated site by testing JavaScript, CSS3, and HTML5 codes to ensuring cross-platform compatibility\n"
+            ]
+        }
+    ],
+    "name": "Fabian Anguiano",
+    "header": "Hi, I'm Fabian",
+    "cta": {
+        "label": "Get my resume",
+        "url": "https://www.linkedin.com/in/fabian-anguiano-0219a5147/"
+    },
+    "testimonials": [],
+    "title": "",
+    "projects": [
+        {
+            "__key": "ceb4588b-3822-4ac6-9d13-f79f91c4f72f",
+            "name": "Macro Manage",
+            "description": " Macro Manage is a static site, that relies on the Edamam API. The front end was built with the bulma frame work. The API is calling using Jquery Ajax. The result are dynamically generated with bootstrap.",
+            "tags": [
+                "HTML",
+                "Javascript",
+                "CSS",
+                "API"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "http://macromanage.net/"
+        },
+        {
+            "__key": "989ae2a4-a334-495d-9afe-9dde06a772f7",
+            "name": "Hexo-Digital",
+            "description": "Hexo-Digital is a digital media agency I run. The website is a landing page built with bootstrap.",
+            "tags": [
+                "HTML",
+                "CSS",
+                "JavaScript",
+                "Bootstrap"
+            ],
+            "img": "merc-01.png",
+            "url": "https://hexodigital.com/"
+        },
+        {
+            "__key": "44dc122b-e3ad-48a3-b6a7-2adc3503902b",
+            "name": "DevConnect",
+            "description": "Full stack social network application using Node.js, Express, React, Redux and MongoDB along with ES6+.\n\n",
+            "tags": [
+                "Node.js",
+                "Express",
+                "React",
+                "React",
+                "Redux",
+                "MongoDB",
+                "ES6"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://github.com/thetrebelcc/MernFullStackSocialSite"
+        }
+    ],
+    "email": "fabiananaguiano@gmail.com",
+    "description": "I'm a freelance developer with 2years of experience, having worked with various clients. I enjoy building everything from small business sites to rich interactive web apps.",
+    "footerTitle": "Full stack developer"
+}
